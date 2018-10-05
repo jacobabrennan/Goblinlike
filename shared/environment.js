a171c2fe7ffee020d3879daa00c7d0ed33253a37
JacobABrennan <jacobabrennan@gmail.com>
2018-10-04 23:39:37 -0400

    Refactored environment.js to use const

--- shared/environment.js
+++ shared/environment.js
@@ -5,87 +5,87 @@
   ===========================================================================*/
 
     // Misc. Configuration:
-var debug = false;
-var VERSION = '1.1.4';
-var displaySize = 21;
-var DEFAULT_MAP_SIZE = 48;
-var HIGHLIGHT = 'highlight';
-var COLOR_INSTRUCTION = '#00f';
-var TILE_SIZE = 8;
-var FONT_SIZE = 8;
-var FINAL_DEPTH = 7;
-var INTRO_TITLE = 'You descend into darkness';
-var INTRO_BODY = 'While running from humans in the forest, you stumble on a secret passage into an abandoned dwarven city. The once magnificent halls and chambers are now littered with trash and debris. It smells of decay. You think you hear the sound of another goblin up ahead, but you think you also saw something hideous crawling in the darkness.';
-var GOBLIN_SCORE = 100;
-var URL_SCORE_REPORT = 'http://jacobabrennan.com:7231/scores';
+const debug = false;
+const VERSION = '1.1.4';
+const displaySize = 21;
+const DEFAULT_MAP_SIZE = 48;
+const HIGHLIGHT = 'highlight';
+const COLOR_INSTRUCTION = '#00f';
+const TILE_SIZE = 8;
+const FONT_SIZE = 8;
+const FINAL_DEPTH = 7;
+const INTRO_TITLE = 'You descend into darkness';
+const INTRO_BODY = 'While running from humans in the forest, you stumble on a secret passage into an abandoned dwarven city. The once magnificent halls and chambers are now littered with trash and debris. It smells of decay. You think you hear the sound of another goblin up ahead, but you think you also saw something hideous crawling in the darkness.';
+const GOBLIN_SCORE = 100;
+const URL_SCORE_REPORT = 'http://jacobabrennan.com:7231/scores';
     // Directions:
-var WAIT      = 0;
-var NORTH     = 1;
-var SOUTH     = 2;
-var EAST      = 4;
-var WEST      = 8;
-var NORTHEAST = 5;
-var NORTHWEST = 9;
-var SOUTHEAST = 6;
-var SOUTHWEST = 10;
-var UP        = 16;
-var DOWN      = 32;
+const WAIT      = 0;
+const NORTH     = 1;
+const SOUTH     = 2;
+const EAST      = 4;
+const WEST      = 8;
+const NORTHEAST = 5;
+const NORTHWEST = 9;
+const SOUTHEAST = 6;
+const SOUTHWEST = 10;
+const UP        = 16;
+const DOWN      = 32;
     // Action Commands:
-var COMMAND_CANCEL      = 64;
-var COMMAND_USE         = 65;
-var COMMAND_NEWGAME     = 66;
-var COMMAND_GET         = 67;
-var COMMAND_DROP        = 68;
-var COMMAND_LOOK        = 69;
-var COMMAND_EQUIP       = 70;
-var COMMAND_UNEQUIP     = 71;
-var COMMAND_STAIRS      = 72;
-var COMMAND_FIRE        = 73;
-var COMMAND_THROW       = 74;
-var COMMAND_MOVE        = 75;
-var COMMAND_WAIT        = 76;
-var COMMAND_HELP        = 77;
-var COMMAND_CLOSE       = 78;
-var COMMAND_PAGEDOWN    = 79;
-var COMMAND_PAGEUP      = 80;
-var COMMAND_ENTER       = 81;
-var COMMAND_NONE        = 82;
-var COMMAND_CAMP        = 83;
-var COMMAND_ATTACK      = 84;
+const COMMAND_CANCEL      = 64;
+const COMMAND_USE         = 65;
+const COMMAND_NEWGAME     = 66;
+const COMMAND_GET         = 67;
+const COMMAND_DROP        = 68;
+const COMMAND_LOOK        = 69;
+const COMMAND_EQUIP       = 70;
+const COMMAND_UNEQUIP     = 71;
+const COMMAND_STAIRS      = 72;
+const COMMAND_FIRE        = 73;
+const COMMAND_THROW       = 74;
+const COMMAND_MOVE        = 75;
+const COMMAND_WAIT        = 76;
+const COMMAND_HELP        = 77;
+const COMMAND_CLOSE       = 78;
+const COMMAND_PAGEDOWN    = 79;
+const COMMAND_PAGEUP      = 80;
+const COMMAND_ENTER       = 81;
+const COMMAND_NONE        = 82;
+const COMMAND_CAMP        = 83;
+const COMMAND_ATTACK      = 84;
     // Commands from server.
-var COMMAND_SENSE   = 100;
-var COMMAND_TURN    = 101;
-var COMMAND_GAMEOVER= 102;
-var COMMAND_WIN     = 103;
+const COMMAND_SENSE   = 100;
+const COMMAND_TURN    = 101;
+const COMMAND_GAMEOVER= 102;
+const COMMAND_WIN     = 103;
     // Targeting system:
-var TARGET_SELF = 1; // Allow the self to be targetted. Will skip selection if this is the only flag set.
-var TARGET_FRIEND = 2; // Allow targeting of friendly actors.
-var TARGET_ENEMY = 4; // Allow enemies to be targetted.
-var TARGET_OTHER = TARGET_ENEMY|TARGET_FRIEND; // Allow any other actor to be targeted.
-var TARGET_ANYONE = TARGET_OTHER|TARGET_SELF; // Allow any actor to be targeted.
-var TARGET_ALL = 8; // All viable targets will be effected, not just one (no selection).
-var TARGET_FURNITURE = 16; // Allow to target furniture.
-var TARGET_RANGE = 32; // Allow targets in range, not just those in view.
-var TARGET_DIRECTION = 64; // The player will be prompted to select a direction.
-var RANGE_VIEW = -1; // Targeting will use the actors view range.
+const TARGET_SELF = 1; // Allow the self to be targetted. Will skip selection if this is the only flag set.
+const TARGET_FRIEND = 2; // Allow targeting of friendly actors.
+const TARGET_ENEMY = 4; // Allow enemies to be targetted.
+const TARGET_OTHER = TARGET_ENEMY|TARGET_FRIEND; // Allow any other actor to be targeted.
+const TARGET_ANYONE = TARGET_OTHER|TARGET_SELF; // Allow any actor to be targeted.
+const TARGET_ALL = 8; // All viable targets will be effected, not just one (no selection).
+const TARGET_FURNITURE = 16; // Allow to target furniture.
+const TARGET_RANGE = 32; // Allow targets in range, not just those in view.
+const TARGET_DIRECTION = 64; // The player will be prompted to select a direction.
+const RANGE_VIEW = -1; // Targeting will use the actors view range.
     // Containable object types:
-var TYPE_CONTAINABLE = 1;
-var TYPE_ITEM = 2;
-var TYPE_ACTOR = 3;
-var TYPE_TRAP = 4;
+const TYPE_CONTAINABLE = 1;
+const TYPE_ITEM = 2;
+const TYPE_ACTOR = 3;
+const TYPE_TRAP = 4;
     // Actor Factions (bit flags):
-var FACTION_GOBLIN = 1;
-var FACTION_PLAYER = FACTION_GOBLIN;
-var FACTION_ENEMY = 2;
+const FACTION_GOBLIN = 1;
+const FACTION_PLAYER = FACTION_GOBLIN;
+const FACTION_ENEMY = 2;
     // Companion AI modes:
-var MODE_FOLLOW = 1;
-var MODE_ATTACK = 2;
-var MODE_RETREAT = 3;
-var MODE_SCAVENGE = 4;
+const MODE_FOLLOW = 1;
+const MODE_ATTACK = 2;
+const MODE_RETREAT = 3;
+const MODE_SCAVENGE = 4;
     // Genders:
-var GENDER_MALE = 'm';
-var GENDER_FEMALE = 'f';
-var GENDER_NONBINARY = 'nb';
+const GENDER_MALE = 'm';
+const GENDER_FEMALE = 'f';
+const GENDER_NONBINARY = 'nb';
 
 
 /*===========================================================================
@@ -98,8 +98,8 @@ if(Object.instantiate){
     Object.instantiate = function (aPrototype){
         if(aPrototype.constructor){
             // Create arguments, minus prototype, to pass to constructor.
-            var cleanArguments = [];
-            for(var argI = 1; argI < arguments.length; argI++){
+            const cleanArguments = [];
+            for(let argI = 1; argI < arguments.length; argI++){
                 cleanArguments.push(arguments[argI]);
             }
             // Call constructor, return new object.
@@ -115,10 +115,10 @@ if(Object.extend){
     console.log('Cannot attach method "extend" to Object.');
 } else{
     Object.extend = function (aPrototype, extention){
-        var valueConfiguration = {};
-        for(var key in extention){
+        const valueConfiguration = {};
+        for(let key in extention){
             if(!extention.hasOwnProperty(key)){ continue;}
-            var keyValue = extention[key];
+            const keyValue = extention[key];
             if(keyValue && keyValue.value){
                 valueConfiguration[key] = keyValue;
                 continue;
@@ -139,8 +139,8 @@ if(Object.extend){
     Useful functions.
   ===========================================================================*/
 
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-var characterIndex = function (character){
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const characterIndex = function (character){
     // Converts a letter to it's position in the alphabet. Returns a number.
     character = character.toLowerCase();
     return alphabet.indexOf(character);
@@ -148,21 +148,21 @@ var characterIndex = function (character){
 
 /*=== Common tasks when dealing with arrays. ================================*/
 
-var pick = function (){
+const pick = function (){
     return arrayPick(arguments);
 };
-var arrayPick = function (sourceArray){
+const arrayPick = function (sourceArray){
     // Returns a randomly chosen element from the source array.
-    var randomIndex = Math.floor(Math.random()*sourceArray.length);
-    var randomElement = sourceArray[randomIndex];
+    const randomIndex = Math.floor(Math.random()*sourceArray.length);
+    const randomElement = sourceArray[randomIndex];
     if(!randomElement){
         console.log("Problem: "+randomIndex+'/'+sourceArray.length);
     }
     return randomElement;
 };
-var arrayRemove = function (sourceArray, element){
+const arrayRemove = function (sourceArray, element){
     // Removes element from sourceArray, if present. Returns undefined.
-    var elementIndex = sourceArray.indexOf(element);
+    const elementIndex = sourceArray.indexOf(element);
     if(elementIndex != -1){
         sourceArray.splice(elementIndex, 1);
     }
@@ -170,49 +170,49 @@ var arrayRemove = function (sourceArray, element){
 
 /*=== Math. =================================================================*/
 
-var randomInterval = function (min, max){
+const randomInterval = function (min, max){
     // Returns a randomly select integer between min and max, inclusive.
     if(!min){ min = 0;}
     if(!max){ max = min; min = 0;}
-    var range = max-min;
+    const range = max-min;
     return min + Math.floor(Math.random()*(range+1));
 };
-var gaussRandom = function (mean, standardDeviation){
+const gaussRandom = function (mean, standardDeviation){
     /**
      *  Generates random integers with a gaussian (normal) distribution about
      *      the specified mean, with the specified standard deviation.
      *  Returns an integer.
      **/
-    var leg1;
-    var leg2;
+    let leg1;
+    let leg2;
     do{
         leg1 = Math.random();
         leg2 = Math.random();
     } while(!(leg1 && leg2));
-    var normal = Math.cos(2*Math.PI*leg2) * Math.sqrt(-(2*Math.log(leg1)));
-    var gaussian = mean + normal*standardDeviation;
+    let normal = Math.cos(2*Math.PI*leg2) * Math.sqrt(-(2*Math.log(leg1)));
+    let gaussian = mean + normal*standardDeviation;
     return Math.round(gaussian);
 };
-var distance = function (startX, startY, endX, endY){
-    var deltaX = Math.abs(endX-startX);
-    var deltaY = Math.abs(endY-startY);
+const distance = function (startX, startY, endX, endY){
+    const deltaX = Math.abs(endX-startX);
+    const deltaY = Math.abs(endY-startY);
     return Math.max(deltaX, deltaY);
 };
-var getStepCoords = function (startX, startY, direction){
+const getStepCoords = function (startX, startY, direction){
     if(direction & NORTH){ startY++;}
     if(direction & SOUTH){ startY--;}
     if(direction & EAST ){ startX++;}
     if(direction & WEST ){ startX--;}
     return {x: startX, y: startY};
 };
-var directionTo = function (startX, startY, endX, endY){
-    var deltaX = endX-startX;
-    var deltaY = endY-startY;
+const directionTo = function (startX, startY, endX, endY){
+    let deltaX = endX-startX;
+    let deltaY = endY-startY;
     if(!deltaX && !deltaY){
         return 0;
     }
-    var direction = 0;
-    var angle = Math.atan2(deltaY, deltaX); // Reversed, don't know why.
+    let direction = 0;
+    let angle = Math.atan2(deltaY, deltaX); // Reversed, don't know why.
     angle /= Math.PI;
     angle /= 2; // Convert to Tau.
     angle += 1/16;
